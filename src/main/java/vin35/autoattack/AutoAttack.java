2450004e0e8e0541721aa38e3ff0c5e3d74c7472
vin350 <53111833+vin350@users.noreply.github.com>
2022-10-10 20:58:05 -0300

    Add attack indicator

--- src/main/java/vin35/autoattack/AutoAttack.java
+++ src/main/java/vin35/autoattack/AutoAttack.java
@@ -3,8 +3,13 @@ package vin35.autoattack;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.projectile.ProjectileUtil;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
@@ -12,6 +17,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import vin35.autoattack.config.AutoAttackConfig;
+import vin35.autoattack.mixin.InGameHudMixin;
 
 
 public class AutoAttack implements ClientModInitializer {
@@ -20,11 +26,33 @@ public class AutoAttack implements ClientModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	//public static final Logger LOGGER = LoggerFactory.getLogger("autoattack");
 
+	static boolean isAttackable = false;
+
 	@Override
 	public void onInitializeClient() {
 		//LOGGER.warn("Hello Fabric world!");
 
 		ClientTickEvents.END_CLIENT_TICK.register(mc -> {
+			//isAttackable
+			if (mc.player != null && mc.crosshairTarget != null) {
+				if (mc.crosshairTarget.getType() == HitResult.Type.BLOCK && AutoAttackConfig.cleanCut) {
+					BlockPos blockPos = new BlockPos(mc.crosshairTarget.getPos().x, mc.crosshairTarget.getPos().y, mc.crosshairTarget.getPos().z);
+					BlockState state = mc.world.getBlockState(blockPos);
+
+					if (state.getCollisionShape(mc.world, blockPos).isEmpty() || state.getHardness(mc.world, blockPos) == 0.0F) {
+						float reach = mc.interactionManager.getReachDistance();
+						Vec3d camera = mc.player.getCameraPosVec(1.0F);
+						Vec3d rotation = mc.player.getRotationVec(1.0F);
+						Vec3d end = camera.add(rotation.x * reach, rotation.y * reach, rotation.z * reach);
+						EntityHitResult result = ProjectileUtil.raycast(mc.player, camera, end, new Box(camera, end), e -> !e.isSpectator() && e.isAttackable(), reach * reach);
+						if (result != null && result.getEntity().isAlive()){
+							isAttackable = true;
+						}else isAttackable = false;
+					}else isAttackable = false;
+				}else isAttackable = false;
+			}
+
+			//auto attack
 			if (mc.options.attackKey.isPressed() && mc.player != null
 					&& mc.player.getAttackCooldownProgress(0) >= 1) {
 				if (mc.crosshairTarget != null) {
@@ -38,7 +66,7 @@ public class AutoAttack implements ClientModInitializer {
 							Vec3d rotation = mc.player.getRotationVec(1.0F);
 							Vec3d end = camera.add(rotation.x * reach, rotation.y * reach, rotation.z * reach);
 							EntityHitResult result = ProjectileUtil.raycast(mc.player, camera, end, new Box(camera, end), e -> !e.isSpectator() && e.isAttackable(), reach * reach);
-							if (result != null){
+							if (result != null && result.getEntity().isAlive()){
 								mc.interactionManager.attackEntity(mc.player, result.getEntity());
 								mc.player.swingHand(Hand.MAIN_HAND);
 							}
@@ -53,5 +81,20 @@ public class AutoAttack implements ClientModInitializer {
 				}
 			}
 		});
+	}
+
+	public static float getProgress(float weaponProgress) {
+		ClientPlayerEntity player = MinecraftClient.getInstance().player;
+		ItemStack mainHand = player.getMainHandStack();
+		ItemStack offHand = player.getOffHandStack();
+
+		if (weaponProgress < 1.0F)
+			return weaponProgress;
+
+		if (isAttackable) {
+			return 2.0F;
+		}
+
+		return 1.0F;
 	}
 }
